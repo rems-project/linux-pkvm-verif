b1cdb018e806ed43b1fff023e6b316d3ee32c169
Will Deacon <willdeacon@google.com>
2020-10-05 17:38:02 +0100

    Revert "kvm: arm64: Move nVHE-only per-CPU state to hyp/nvhe"
    This reverts commit 2973fa1d79471cbe6aca2549ee3c43ba22ca99e5.
    
    Signed-off-by: Will Deacon <willdeacon@google.com>

--- arch/arm64/kvm/hyp/nvhe/switch.c
+++ arch/arm64/kvm/hyp/nvhe/switch.c
@@ -28,9 +28,6 @@
 #include <asm/processor.h>
 #include <asm/thread_info.h>
 
-DEFINE_PER_CPU(struct kvm_cpu_context, kvm_hyp_ctxt);
-DEFINE_PER_CPU(struct kvm_vcpu, kvm_host_vcpu);
-
 static void __activate_traps(struct kvm_vcpu *vcpu)
 {
 	u64 val;
